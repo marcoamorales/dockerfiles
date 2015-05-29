0c8b0aca7985a816a4ec04ec451ff36365c28fc9
Marco A. Morales <marcoamorales@gmail.com>
2015-05-28 23:03:26 -0500

    hardcode the user

--- subsonic/start.sh
+++ subsonic/start.sh
@@ -2,17 +2,7 @@
 
 set -e
 
-SUBSONIC_USER=subsonic
-SUBSONIC_USER_UID=2001
-
-if [ $SUBSONIC_USER ]; then
-  if [ $SUBSONIC_USER_UID ]; then
-    useradd -u $SUBSONIC_USER_UID $SUBSONIC_USER
-  else
-    useradd $SUBSONIC_USER
-  fi
-
-  sed -i "s/\${JAVA}/sudo -u $SUBSONIC_USER \${JAVA}/" /usr/share/subsonic/subsonic.sh
+  sed -i "s/\${JAVA}/sudo -u xmedia \${JAVA}/" /usr/share/subsonic/subsonic.sh
 fi
 
 /usr/share/subsonic/subsonic.sh
