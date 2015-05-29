433f4d16a87b973a86c267d773b293ad12e52ba7
Marco A. Morales <marcoamorales@gmail.com>
2015-05-28 23:41:36 -0500

    fix leftover fi

--- subsonic/start.sh
+++ subsonic/start.sh
@@ -2,7 +2,6 @@
 
 set -e
 
-  sed -i "s/\${JAVA}/sudo -u xmedia \${JAVA}/" /usr/share/subsonic/subsonic.sh
-fi
+sed -i "s/\${JAVA}/sudo -u xmedia \${JAVA}/" /usr/share/subsonic/subsonic.sh
 
 /usr/share/subsonic/subsonic.sh
